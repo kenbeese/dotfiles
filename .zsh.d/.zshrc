dc7700c071e4d2f8900f5f7b46c35dc3f27574fc
TAKAGI Kentaro <kentaro0910+git@gmail.com>
2015-06-23 06:44:01 +0900

    Add alias.zsh

---
+++ .zsh.d/.zshrc
@@ -0,0 +1,1 @@
+[ -f ~/.zsh.d/alias.zsh ] && source ~/.zsh.d/alias.zsh
---
+++ .zsh.d/alias.zsh
@@ -0,0 +1,73 @@
+# 補完される前にオリジナルのコマンドまで展開してチェックする
+setopt complete_aliases
+
+set_alias(){
+    alias grep='grep --color'
+    alias pag="ps aux |grep"
+    alias ll='ls -lh'
+    alias la='ls -a'
+    alias pkill='pkill -x'
+    alias lv='lv -c'
+    alias less='less -r'
+    alias le='less -r'
+    alias ne="emacsclient -nw"
+    alias e="emacsclient -n"
+    alias em="emacsclient"
+    function read_only_emacsclient() {
+        emacsclient -t -e "(find-file-read-only \"$1\")"
+    }
+    alias vw="read_only_emacsclient"
+
+    alias -g L='| less'
+    alias -g H='| head'
+    alias -g T='| tail'
+    alias -g G='| grep'
+    alias -g S='| sed'
+    alias -g A='| awk'
+    alias -g W='| wc'
+}
+
+set_gnu_alias(){
+    alias emacsserver="XMODIFIERS=@im=none emacs"
+    alias xlock="xlock -mode blank"
+    alias pxdvi='LANG=C pxdvi'
+    alias gv='LANG=C gv'
+    alias ls="ls --color"
+}
+
+set_darwin_alias(){
+    alias javac="javac -J-Dfile.encoding=UTF8"
+    alias java="java -Dfile.encoding=UTF8"
+    alias ls="gls --color"
+    alias more='more -r'
+    alias emacsserver="open -a emacs -n"
+    alias emacs="/usr/local/bin/emacsclient -n"
+    alias emacsclient="/usr/local/bin/emacsclient"
+    alias acroread='open -a Adobe\ reader -n'
+    alias vlc='open -a vlc'
+}
+set_solaris_alias(){}
+set_linux_alias(){}
+
+
+set_os_alias() {
+    case $OSTYPE in
+      *darwin*)
+        set_darwin_alias
+        ;;
+      *solaris*)
+        set_gnu_alias
+        set_solaris_alias
+        ;;
+      *)
+        set_gnu_alias
+        set_linux_alias
+        ;;
+    esac
+}
+
+# For default alias
+set_alias
+
+# For OS depending alias
+set_os_alias
