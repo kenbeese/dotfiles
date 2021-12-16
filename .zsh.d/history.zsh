afcd3ea16829946b3712a02ce7df0f1c84b13a3c
TAKAGI Kentaro <kentaro0910+git@gmail.com>
2021-12-16 13:26:30 +0900

    Update

--- .zsh.d/history.zsh
+++ .zsh.d/history.zsh
@@ -10,10 +10,11 @@ setopt hist_ignore_dups     # ignore duplication command history list
 setopt share_history        # share command history data
 setopt hist_ignore_all_dups  # 重複するコマンド行は古い方を削除
 setopt append_history        # 履歴を追加 (毎回 .zsh_history を作るのではなく)
-setopt inc_append_history    # 履歴をインクリメンタルに追加
+#setopt inc_append_history    # 履歴をインクリメンタルに追加 share_historyと一緒に使わない
 setopt hist_no_store         # historyコマンドは履歴に登録しない
 setopt hist_reduce_blanks    # 余分な空白は詰めて記録
 setopt hist_ignore_space     # 先頭がスペースの場合、ヒストリに追加しない
+setopt hist_fcntl_lock       # fcntlのロックを使う
 
 # auto directory pushd that you can get dirs list by cd -[tab]
 setopt auto_pushd
